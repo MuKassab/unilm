ee8c565e72c74df19e63d64813b3d8dd5f021823
Muhammad M. Kassab <muhammadkassab97@yahoo.com>
2020-04-06 12:12:40 +0200

    Update seq2seq_loader.py

--- unilm-v1/src/biunilm/seq2seq_loader.py
+++ unilm-v1/src/biunilm/seq2seq_loader.py
@@ -3,7 +3,10 @@ from random import random as rand
 import math
 import torch
 
-from biunilm.loader_utils import get_random_word, batch_list_to_batch_tensors, Pipeline
+import sys
+sys.path.append("/content/unilm/unilmv1")
+
+from src.biunilm.loader_utils import get_random_word, batch_list_to_batch_tensors, Pipeline
 
 # Input file format :
 # 1. One sentence per line. These should ideally be actual sentences,
