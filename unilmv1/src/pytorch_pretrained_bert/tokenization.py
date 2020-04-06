da5686d7b285f9321c43a4c0f95b0b1310369feb
Muhammad M. Kassab <muhammadkassab97@yahoo.com>
2020-04-06 13:47:56 +0200

    Update tokenization.py

--- unilmv1/src/pytorch_pretrained_bert/tokenization.py
+++ unilmv1/src/pytorch_pretrained_bert/tokenization.py
@@ -114,7 +114,8 @@ class BertTokenizer(object):
         """Converts a sequence of tokens into ids using the vocab."""
         ids = []
         for token in tokens:
-            ids.append(self.vocab[token])
+            if(token in self.vocab):
+                ids.append(self.vocab[token])
         if len(ids) > self.max_len:
             raise ValueError(
                 "Token indices sequence length is longer than the specified maximum "
